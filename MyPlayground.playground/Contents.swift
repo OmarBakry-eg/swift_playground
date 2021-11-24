d380e1ed977a4bc290d4a09a53a4c38ea175788a
OmarBakry-eg <OmarBakry202020@gmail.com>
2021-11-24 15:12:48 +0200

    Day 13 – protocols, extensions

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,133 +1,120 @@
-// classes called by reference look for game example
-// let objs can modifiy it's property, look for carDev obj and its class
 
-class Game {
-    var score : Int = 0
-    func  printScore()  {
-     print(score)
+
+// protocols = abstraction
+protocol Vehicle {
+    var name: String { get }
+    var currentPassengers: Int { get set }
+    func estimateTime(for distance: Int) -> Int
+    func travel(distance: Int)
+}
+
+struct Bicycle: Vehicle {
+    var name = "Bicycle"
+    var currentPassengers = 1
+    func estimateTime(for distance: Int) -> Int {
+        distance / 10
+    }
+
+    func travel(distance: Int) {
+        print("I'm cycling \(distance)km.")
     }
 }
 
-var newGame = Game()
-var mainGame = newGame
-newGame.score = 10
-mainGame.score = 30
-newGame.printScore() //30
-mainGame.printScore()//30
-// You shared same data from the class and if one change the rest of objs that share same data should be change.
-print("----------------------------------")
-// Inhertience without init
-class Emp {
-    let hours:Int
-    init(hours : Int) {
-        self.hours = hours
+struct Car: Vehicle {
+    let name = "Car"
+    var currentPassengers = 1
+    func estimateTime(for distance: Int) -> Int {
+        distance / 50
     }
-    func printSummary(_ workType:String){
-        print("I'm a \(workType), I work \(hours) hours per day")
+
+    func travel(distance: Int) {
+        print("I'm driving \(distance)km.")
+    }
+
+    func openSunroof() {
+        print("It's a nice day!")
     }
 }
 
-class Developer : Emp {}
-
-class Worker : Emp {}
-
-let dev = Developer(hours: 8)
-let worker = Worker(hours: 5)
-dev.printSummary("dev")
-worker.printSummary("worker")
-
-print("----------------------------------")
-
-// Inhertience with init
-
-class Vehicle{
-    let isElectric : Bool
-    init(isElectric : Bool) {
-        self.isElectric = isElectric
-    }
-}
-class CarForDev : Vehicle {
-    var isConvertible : Bool // let objs can modifiy it's property
-    init(isConvertible: Bool, isElectric:Bool) {
-        self.isConvertible = isConvertible
-        super.init(isElectric: isElectric)
-    }
-}
-class CarForWorker : Vehicle {
-    let isConvertible : Bool = false
-}
-
-let carDev = CarForDev(isConvertible: true, isElectric: true)
-carDev.isConvertible = false //let objs can modifiy it's property
-let carWorker = CarForWorker(isElectric: true)
-
-//---------------------------
-
-// Copy Classes
-
-class User {
-    var username:String = "Omar"
-    func copy() -> User {
-        let user = User()
-        user.username = username
-        return user
+func commute(distance: Int, using vehicle: Vehicle) {
+    if vehicle.estimateTime(for: distance) > 100 {
+        print("That's too slow! I'll try a different vehicle.")
+    } else {
+        vehicle.travel(distance: distance)
     }
 }
 
-var user1 = User()
-var user2 = user1.copy()
-user2.username = "Bakry"
-print(user1.username)
-print(user2.username)
-
-print("----------------------------------")
-
-// deinit
-
-class DeInitTest{
-    let id : Int
-    init(id:Int) {
-        self.id = id
-        print("User \(id) : I'm alive")
-    }
-    
-    deinit {
-        print("User \(id) : I'm dead")
+func getTravelEstimates(using vehicles: [Vehicle], distance: Int) {
+    for vehicle in vehicles {
+        let estimate = vehicle.estimateTime(for: distance)
+        print("\(vehicle.name): \(estimate) hours to travel \(distance)km")
     }
 }
 
-for i in 1...3 {
-    let user = DeInitTest(id: i)
-    print("User \(user.id) : I'm in control")
-//    User 1 : I'm alive
-//    User 1 : I'm in control
-//    User 1 : I'm dead
-//    User 2 : I'm alive
-//    User 2 : I'm in control
-//    User 2 : I'm dead
-//    User 3 : I'm alive
-//    User 3 : I'm in control
-//    User 3 : I'm dead
-}
-print("----------------------------------")
-var users :[DeInitTest] = [DeInitTest]()
-for i in 1...3{
-   
-    let user = DeInitTest(id: i)
-    print("User \(user.id) : I'm in control")
-    users.append(user)
+let car = Car()
+commute(distance: 100, using: car)
+
+var bike = Bicycle()
+commute(distance: 50, using: bike)
+
+getTravelEstimates(using: [car, bike], distance: 150)
+
+print("---------------------------")
+
+// Opaque return types
+// idea here we need to return a protocol and equalaize the two retrun types from one func , so we return a public protocol Equatable, but as we know protocol can be adobted by a lot of children So we need just a `SOME` from them that mean not all of things but just its specifc return data type
+func getRandomNum() -> some Equatable{
+  //  Int.random(in: 1...6)
+    Double.random(in: 1...6) // you could retrun anything here
 }
 
-users.removeAll()
-//User 1 : I'm alive
-//User 1 : I'm in control
-//User 2 : I'm alive
-//User 2 : I'm in control
-//User 3 : I'm alive
-//User 3 : I'm in control
-//User 1 : I'm dead
-//User 2 : I'm dead
-//User 3 : I'm dead
-print("Array cleared !")
-//Users don't destroy immediatly because they're stored in array
-print("----------------------------------")
+func getRandomBool() -> some Equatable{
+    Bool.random()
+}
+
+print(getRandomNum() == getRandomNum()) // false
+print("---------------------------")
+
+//extensions
+struct Book {
+    let title: String
+    let pageCount: Int
+    let readingHours: Int
+}
+
+extension Book {
+    // If I put it in main struct it will throw an err because if we defined a custom init we've to define first the default init , so if we don't then extension can do it perfectly
+    init(title: String, pageCount: Int) {
+        self.title = title
+        self.pageCount = pageCount
+        self.readingHours = pageCount / 50
+    }
+}
+
+let lotr = Book(title: "Lord of the Rings", pageCount: 1178, readingHours: 24)
+let swfitBook = Book(title: "Swift Book", pageCount: 600)
+
+
+protocol Person {
+    var name: String { get }
+    func sayHello()
+}
+
+extension Person { // protocol extension ..
+// if I did not define the func sayHello here it will caught an exp in Employee class but If I defined it could be a default implementation to it
+    func sayHello() {
+           print("Hi, I'm \(name)")
+       }
+}
+
+struct Employee: Person {
+    let name: String
+    // I could override it or not
+//    func sayHello() {
+//        <#code#>
+//    }
+}
+
+let taylor = Employee(name: "Omar Bakry")
+taylor.sayHello()
+// --------------------------------
