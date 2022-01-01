e6f15cb4e84f95f71f47eb42a81a0d5bd3f6d1e0
OmarBakry-eg <OmarBakry202020@gmail.com>
2022-01-01 14:31:37 +0200

    linked list imp

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,120 +1,194 @@
 
-
-// protocols = abstraction
-protocol Vehicle {
-    var name: String { get }
-    var currentPassengers: Int { get set }
-    func estimateTime(for distance: Int) -> Int
-    func travel(distance: Int)
-}
-
-struct Bicycle: Vehicle {
-    var name = "Bicycle"
-    var currentPassengers = 1
-    func estimateTime(for distance: Int) -> Int {
-        distance / 10
-    }
-
-    func travel(distance: Int) {
-        print("I'm cycling \(distance)km.")
+class Node {
+    var value: Int
+    var next: Node?
+    
+    init(value: Int) {
+        self.value = value
     }
 }
 
-struct Car: Vehicle {
-    let name = "Car"
-    var currentPassengers = 1
-    func estimateTime(for distance: Int) -> Int {
-        distance / 50
+class LinkedList {
+    
+    var head: Node?
+    
+    init(head: Node?) {
+        self.head = head
     }
+    
+    func append(_ node: Node) {
 
-    func travel(distance: Int) {
-        print("I'm driving \(distance)km.")
+        guard head != nil else {
+            head = node
+            return
+        }
+        
+        var current = head
+        while let _ = current?.next {
+            current = current?.next
+        }
+        current?.next = node
     }
+    
+    // Get a node from a particular position.
+    // Assume the first position is "1".
+    // Return "nil" if position is not in the list.
+    func getNode(atPosition position: Int) -> Node? {
+        guard position > 0 else {
+                return nil
+            }
 
-    func openSunroof() {
-        print("It's a nice day!")
+            var counter = 1
+            var current = head
+
+            while current != nil && counter <= position {
+                if counter == position {
+                    return current
+                }
+                current = current?.next
+                counter += 1
+            }
+            return nil
     }
-}
-
-func commute(distance: Int, using vehicle: Vehicle) {
-    if vehicle.estimateTime(for: distance) > 100 {
-        print("That's too slow! I'll try a different vehicle.")
-    } else {
-        vehicle.travel(distance: distance)
+    
+    // Insert a new node at the given position.
+    // Assume the first position is "1".
+    // Inserting at position 3 means between
+    // the 2nd and 3rd nodes.
+    func insertAtFirst(_ node : Node?){
+        let newNode = node
+        let current = head
+        if current != nil {
+            newNode?.next = current
+            head = newNode
+        } else {
+            head = newNode
+        }
     }
-}
-
-func getTravelEstimates(using vehicles: [Vehicle], distance: Int) {
-    for vehicle in vehicles {
-        let estimate = vehicle.estimateTime(for: distance)
-        print("\(vehicle.name): \(estimate) hours to travel \(distance)km")
+    
+    func inserAtLast(_ node : Node?){
+        let newNode = node
+        var current = head
+        while current?.next != nil{
+            current = current?.next
+        }
+        current?.next = newNode
+        newNode?.next = nil
     }
-}
+    
+    func insertNode(_ node: Node, at position: Int) {
+        guard position > 0 else {
+            return
+        }
 
-let car = Car()
-commute(distance: 100, using: car)
+        var counter = 1
+        var current = head
 
-var bike = Bicycle()
-commute(distance: 50, using: bike)
-
-getTravelEstimates(using: [car, bike], distance: 150)
-
-print("---------------------------")
-
-// Opaque return types
-// idea here we need to return a protocol and equalaize the two retrun types from one func , so we return a public protocol Equatable, but as we know protocol can be adobted by a lot of children So we need just a `SOME` from them that mean not all of things but just its specifc return data type
-func getRandomNum() -> some Equatable{
-  //  Int.random(in: 1...6)
-    Double.random(in: 1...6) // you could retrun anything here
-}
-
-func getRandomBool() -> some Equatable{
-    Bool.random()
-}
-
-print(getRandomNum() == getRandomNum()) // false
-print("---------------------------")
-
-//extensions
-struct Book {
-    let title: String
-    let pageCount: Int
-    let readingHours: Int
-}
-
-extension Book {
-    // If I put it in main struct it will throw an err because if we defined a custom init we've to define first the default init , so if we don't then extension can do it perfectly
-    init(title: String, pageCount: Int) {
-        self.title = title
-        self.pageCount = pageCount
-        self.readingHours = pageCount / 50
+        if position > 1 {
+            while current != nil && counter < position {
+                if counter == position - 1 {
+                    node.next = current?.next
+                    current?.next = node
+                    break
+                }
+                current = current?.next
+                counter += 1
+            }
+        } else if position == 1 {
+            node.next = head
+            head = node
+        }
     }
+    
+    func deleteLast(){
+        var current = head
+        var prev : Node?
+        while current?.next != nil {
+            prev = current
+            current = current?.next
+        }
+        prev?.next = nil
+    }
+    
+    func deleteFirst(){
+        let current = head
+        if current != nil {
+            head = current?.next
+            current?.next = nil
+        }
+    }
+    // Delete the first node with a given value.
+    func deleteNode(withValue value: Int) {
+        var current = head
+        var prev : Node?
+        
+        while current?.value != value && current?.next != nil {
+            prev = current
+            current = current?.next
+        }
+        
+        if current?.value == value {
+            if prev != nil {
+                prev?.next = current?.next
+                current?.next = nil
+            }else {
+                head = current?.next
+                current?.next = nil
+            }
+        }
+    }
+    
+    func printVal(){
+        var current = head
+        while current != nil {
+            print(current?.value ?? "Nothing")
+            current = current?.next
+        }
+    }
+    
+
+    
 }
 
-let lotr = Book(title: "Lord of the Rings", pageCount: 1178, readingHours: 24)
-let swfitBook = Book(title: "Swift Book", pageCount: 600)
+// Test cases
 
+// Set up some Nodes
+let n1 = Node(value: 1)
+let n2 = Node(value: 2)
+let n3 = Node(value: 3)
+let n4 = Node(value: 4)
+let n5 = Node(value: 5)
+let n6 = Node(value: 6)
+// Start setting up a LinkedList
+let ll = LinkedList(head: n1)
+ll.append(n2)
+ll.append(n3)
+ll.insertAtFirst(n5)
+ll.inserAtLast(n6)
+ll.printVal()
+print("-------------------")
+ll.deleteLast()
+ll.deleteFirst()
+ll.printVal()
+print("-------------------")
+ll.deleteNode(withValue: 1)
+ll.printVal()
+// Test getNode(atPosition:)
+//print(ll.head?.next?.next?.value ?? "nothing") // Should print 3
+//print(ll.getNode(atPosition: 3)?.value ?? "nothig")
+//
+////
+//// Test insert
+//print(ll.head?.next?.next?.value ?? "nothing")
 
-protocol Person {
-    var name: String { get }
-    func sayHello()
-}
+//ll.insertNode(n4, at: 3)
+//print(ll.head?.next?.next?.value ?? "nothing")
+//print(ll.getNode(atPosition: 3)!.value) // Should print 4 now
 
-extension Person { // protocol extension ..
-// if I did not define the func sayHello here it will caught an exp in Employee class but If I defined it could be a default implementation to it
-    func sayHello() {
-           print("Hi, I'm \(name)")
-       }
-}
+//ll.printVal()
+//// Test delete(withValue:)
+//ll.deleteNode(withValue: 1)
+//print(ll.getNode(atPosition: 1)!.value) // Should print 2 now
+//print(ll.getNode(atPosition: 2)!.value) // Should print 4 now
+//print(ll.getNode(atPosition: 3)!.value) // Should print 3
 
-struct Employee: Person {
-    let name: String
-    // I could override it or not
-//    func sayHello() {
-//        <#code#>
-//    }
-}
-
-let taylor = Employee(name: "Omar Bakry")
-taylor.sayHello()
-// --------------------------------
