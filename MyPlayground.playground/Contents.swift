a21852165dbef7843e47b22483b82f0a12e01899
OmarBakry-eg <OmarBakry202020@gmail.com>
2022-01-02 22:37:17 +0200

    quick sort

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,52 +1,22 @@
 
 
-func mergeSort(arr : [Int]) -> [Int] {
-    guard arr.count > 1 else {
-        return arr
-    }
-    let middleIndex = arr.count / 2
-    let leftArray = mergeSort(arr: Array(arr[0..<middleIndex]))
-    let rightArray = mergeSort(arr: Array(arr[middleIndex..<arr.count]))
-    return merge(leftArr: leftArray, rightArr: rightArray)
+func quicksort(a: [Int]) -> [Int]{
+    guard a.count > 1 else { return a }
+
+     let pivot = a[a.count/2] // middle index for best case time O(n log n)
+     let less = a.filter { $0 < pivot } // return an arr of numbers that less than pivot ( Spliting )
+     let equal = a.filter { $0 == pivot } // return an arr of numbers that are equal pivot
+     let greater = a.filter { $0 > pivot } // return an arr of numbers that bigger than pivot ( Spliting )
+
+    return quicksort(a:less) + equal + quicksort(a:greater)
+    // return recursively for less and bigger arrays to generate a new pivot for them and sort them for low to high and finally collect them all within one array
 }
 
-func merge(leftArr: [Int] , rightArr : [Int]) -> [Int] {
-    var leftIndex = 0 , rightIndex = 0
-    var sortedArray = [Int]()
-    sortedArray.reserveCapacity(leftArr.count + rightArr.count) // to ensure its size
-    
-    while leftIndex < leftArr.count && rightIndex < rightArr.count {
-        if leftArr[leftIndex] < rightArr[rightIndex] {
-            sortedArray.append(leftArr[leftIndex])
-            leftIndex += 1
-        }else if leftArr[leftIndex] > rightArr[rightIndex] {
-            sortedArray.append(rightArr[rightIndex]) // we always append the smaller one first
-            rightIndex += 1
-        } else { // they're equal
-            sortedArray.append(leftArr[leftIndex])
-            leftIndex += 1
-            sortedArray.append(rightArr[rightIndex]) // we always append the smaller one first
-            rightIndex += 1
-        }
-    }
-    
-    
-    // it means that either the leftArr or the rightArr has its contents completely merged into the sortedArray. At this point, you no longer need to do comparisons. Just append the rest of the contents of the other array until there is no more to append
-    while leftIndex < leftArr.count {
-        sortedArray.append(leftArr[leftIndex])
-        leftIndex += 1
-    }
-    
-    while rightIndex < rightArr.count {
-        sortedArray.append(rightArr[rightIndex])
-        rightIndex += 1
-    }
-    
-    return sortedArray
-}
+//print([1] + [2]) // [1,2]
 
-print(mergeSort(arr: [6,7,9,2,7,8,1,3,0]))
+print(quicksort(a: [6,7,9,2,7,8,1,3,0]))
 
 
-// time : O(nlog(n))
-// space : O(n) it worse that bubble sort in space because bubble sort O(1) it did not need any extra arrays
+// time : O(nlog(n)) for best, worst : O(n^2)
+// worst case happened when we didn't split the arry and choose value-value for sorting
+// space : O(1)
