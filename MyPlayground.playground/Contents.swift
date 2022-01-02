e14b9d3e79ff80e33b13aaa2507331f2f03fba35
OmarBakry-eg <OmarBakry202020@gmail.com>
2022-01-02 21:53:58 +0200

    merge sort

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,19 +1,52 @@
 
 
-func bubbleSort(arr:[Int]) -> [Int] {
-    var array = arr
-    for _ in 0..<array.count {
-        for j in 1..<array.count {
-            if array[j] < array[j-1]{
-               let temp = array[j-1]
-                array[j-1] = array[j]
-                array[j] = temp
-            }
-        }
+func mergeSort(arr : [Int]) -> [Int] {
+    guard arr.count > 1 else {
+        return arr
     }
-    return array
+    let middleIndex = arr.count / 2
+    let leftArray = mergeSort(arr: Array(arr[0..<middleIndex]))
+    let rightArray = mergeSort(arr: Array(arr[middleIndex..<arr.count]))
+    return merge(leftArr: leftArray, rightArr: rightArray)
 }
 
-print(bubbleSort(arr: [4,7,8,2,1,5,8,9,0]))
+func merge(leftArr: [Int] , rightArr : [Int]) -> [Int] {
+    var leftIndex = 0 , rightIndex = 0
+    var sortedArray = [Int]()
+    sortedArray.reserveCapacity(leftArr.count + rightArr.count) // to ensure its size
+    
+    while leftIndex < leftArr.count && rightIndex < rightArr.count {
+        if leftArr[leftIndex] < rightArr[rightIndex] {
+            sortedArray.append(leftArr[leftIndex])
+            leftIndex += 1
+        }else if leftArr[leftIndex] > rightArr[rightIndex] {
+            sortedArray.append(rightArr[rightIndex]) // we always append the smaller one first
+            rightIndex += 1
+        } else { // they're equal
+            sortedArray.append(leftArr[leftIndex])
+            leftIndex += 1
+            sortedArray.append(rightArr[rightIndex]) // we always append the smaller one first
+            rightIndex += 1
+        }
+    }
+    
+    
+    // it means that either the leftArr or the rightArr has its contents completely merged into the sortedArray. At this point, you no longer need to do comparisons. Just append the rest of the contents of the other array until there is no more to append
+    while leftIndex < leftArr.count {
+        sortedArray.append(leftArr[leftIndex])
+        leftIndex += 1
+    }
+    
+    while rightIndex < rightArr.count {
+        sortedArray.append(rightArr[rightIndex])
+        rightIndex += 1
+    }
+    
+    return sortedArray
+}
 
-// O(n^2)
+print(mergeSort(arr: [6,7,9,2,7,8,1,3,0]))
+
+
+// time : O(nlog(n))
+// space : O(n) it worse that bubble sort in space because bubble sort O(1) it did not need any extra arrays
